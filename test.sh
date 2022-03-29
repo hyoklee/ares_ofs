f33dfb9f48f1a21d3302f5279049bbf8ae532248
H. Joe Lee <hyoklee@hdfgroup.org>
2022-03-29 00:06:40 -0500

    Fix burst buffer setting. Grep only user's storage from squeue.

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ date
 # Check if nodes are allocated.
 if [[ -z "${SLURM_NODELIST}" ]]; then
   echo "Allocating nodes."
-  salloc -N 2 --partition compute -w ares-comp-[05,06] --exclusive
+  salloc -N 2 --partition compute -w ares-comp-[29,30] --exclusive
 else
   echo "${SLURM_NODELIST}"
 fi
@@ -34,8 +34,10 @@ mpirun -n 2 -ppn 1 hostname > $NND/client_nodes
 sed -e 's/$/-40g/' -i $NND/client_nodes
 cat $NND/client_nodes
 
-START=`squeue | grep storage | cut -d ']' -f 1 | cut -d '[' -f 2 | cut -d '-' -f 1`
-STOP=`squeue | grep storage | cut -d ']' -f 1 | cut -d '[' -f 2 | cut -d '-' -f 2`
+START=`squeue | grep storage | grep hyoklee |  cut -d ']' -f 1 | cut -d '[' -f 2 | cut -d '-' -f 1`
+echo $START
+STOP=`squeue | grep storage | grep hyoklee | cut -d ']' -f 1 | cut -d '[' -f 2 | cut -d '-' -f 2`
+echo $STOP
 rm $NND/server_nodes
 for n in $(seq $START $STOP) ; do
     i=$(printf "%02d" $n)
@@ -46,6 +48,7 @@ cat $NND/server_nodes
 export PFS_SCRIPTS_DIR=$HOME/ares_ofs
 echo $PFS_SCRIPTS_DIR
 cp $NND/client_nodes $PFS_SCRIPTS_DIR/client_nodes
+cp $NND/server_nodes $PFS_SCRIPTS_DIR/bb_server_nodes
 cp $NND/server_nodes $PFS_SCRIPTS_DIR/pfs_server_nodes
 export ORANGEFS_PATH=/opt/ohpc/pub/orangefs
 export ORANGEFS_KO=${ORANGEFS_PATH}/lib/modules/3.10.0-862.el7.x86_64/kernel/fs/pvfs2/pvfs2.ko
